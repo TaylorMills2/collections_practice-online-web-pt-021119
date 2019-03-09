89afe4af01d6ac5f7230e06c87892936f067a2ca
Taylor Mills <taylormills24@yahoo.com>
2019-03-09 22:12:09 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,11 @@
+ef add_s_collect(array)
+    array.each_with_index.collect do |word,index|
+       if index != 1 
+         word = word + "s"
+        else
+          word = word
+        end
+  end
+end
+
 
